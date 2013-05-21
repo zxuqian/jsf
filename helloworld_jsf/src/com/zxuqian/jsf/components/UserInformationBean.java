d0fd255d80b3b98937de676c14683609f3ffe21b
zxuqian <zxuqian@gmail.com>
2013-05-21 21:14:11 +0800

    jsf components

---
+++ helloworld_jsf/src/com/zxuqian/jsf/components/UserInformationBean.java
@@ -0,0 +1,69 @@
+package com.zxuqian.jsf.components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class UserInformationBean {
+	
+	//inputText
+	private String username;
+	//inputSecret
+	private String password;
+	//inputTextArea
+	private String description;
+	//selectOneRadio
+	private String gender;
+	//selectManyCheckbox
+	private List<String> hobbies;
+	//h:selectOneMenu
+	private String country;
+	
+	public UserInformationBean() {
+		hobbies = new ArrayList<>();
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public List<String> getHobbies() {
+		return hobbies;
+	}
+	public void setHobbies(List<String> hobbies) {
+		this.hobbies = hobbies;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+
+}
